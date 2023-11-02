ff1d946c80124e877f7f1772f4de44dd937e48de
Joaquin Ferroni <joaco.790@gmail.com>
2023-11-02 16:38:35 +0100

    fix crypt issue

--- src/Infrastructure/Appointment.Infrastructure/Security/Crypt.cs
+++ src/Infrastructure/Appointment.Infrastructure/Security/Crypt.cs
@@ -18,20 +18,21 @@ namespace Appointment.Infrastructure.Security
         public Crypt(IOptions<AuthOptions> authOptions)
             => (_authConfig)
                 = (authOptions.Value);
+
         public string DecryptStringFromBytes_Aes(string password)
         {
-            var cipherBytes = Convert.FromBase64String(password);
+            var cipherBytes = Convert.FromBase64String(password.Trim());
             using Aes encryptor = Aes.Create();
             var salt = cipherBytes.Take(16).ToArray();
             var iv = cipherBytes.Skip(16).Take(16).ToArray();
             var encrypted = cipherBytes.Skip(32).ToArray();
-            var pdb = new Rfc2898DeriveBytes(_authConfig.HashValue, salt, 100);
+            var pdb = new Rfc2898DeriveBytes(_authConfig.HashValue, salt, 100, HashAlgorithmName.SHA256);
             encryptor.Key = pdb.GetBytes(32);
             encryptor.Padding = PaddingMode.PKCS7;
             encryptor.Mode = CipherMode.CBC;
             encryptor.IV = iv;
-            using MemoryStream ms = new MemoryStream(encrypted);
-            using CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Read);
+            using var ms = new MemoryStream(encrypted);
+            using var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Read);
             using var reader = new StreamReader(cs, Encoding.UTF8);
             return reader.ReadToEnd();
         }
